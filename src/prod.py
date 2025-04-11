4a15deb4f24e494c0bb227cdd01918ea69d32eab
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-04-11 21:10:05 +0200

    feat: add Flask app for model predictions with simple API interface

---
+++ src/prod.py
@@ -0,0 +1,26 @@
+import dill
+from flask import Flask, request, jsonify
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from features import FeatureExtraction
+import numpy as np
+from model import decode_prediction
+from text_cleaner import preprocess_tokens
+
+with open('out/model.pkl', 'rb') as f:
+    model = dill.load(f)
+
+    app = Flask(__name__)
+
+    @app.route('/predict', methods=['POST'])
+    def predict():
+        data = request.get_json(force=True)
+        title = data['title']
+        channel_name = data['channel_name']
+        grouped_results = model.predict(title, channel_name)
+        return jsonify({
+            'prediction': grouped_results,
+            'status': 'success'
+        })
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000, debug=True)
