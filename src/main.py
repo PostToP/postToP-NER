1acdc4f793a155a98a34041adf177dfbb13c2eae
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-04-29 20:29:58 +0200

    wrap main and prod in a function

--- src/main.py
+++ src/main.py
@@ -13,128 +13,130 @@ import pandas as pd
 VOCAB_SIZE = 50
 MAX_SEQUENCE_LENGTH = 45
 
-dataset = pd.read_json("dataset/data.json")
-dataset = dataset[dataset["NER"].isna() == False]
-dataset = dataset[['Channel Name', 'Title', 'NER', 'Description']]
+
+def main():
+    dataset = pd.read_json("dataset/data.json")
+    dataset = dataset[dataset["NER"].isna() == False]
+    dataset = dataset[['Channel Name', 'Title', 'NER', 'Description']]
+
+    dataset = fix_dataset_NER(dataset)
+
+    ner_tags = convert_ner_tags(dataset["Title"].values, dataset["NER"].values)
+    dataset["NER"] = ner_tags
+
+    title_tokenizer = TokenizerCustom()
+    dataset["Original Tokens"] = dataset["Title"].apply(
+        lambda x: title_tokenizer.encode(x))
+    dataset["Tokens"] = dataset["Original Tokens"].apply(
+        lambda x: preprocess_tokens(x))
+
+    ner_vectorizer = VectorizerNER(MAX_SEQUENCE_LENGTH)
+    ner_vectorizer.train(dataset["NER"].values)
+    dataset["NER"] = dataset["NER"].apply(
+        lambda x: ner_vectorizer.encode(x))
+
+    features = []
+
+    feature_channel = TensorMonad(
+        (dataset["Original Tokens"].values, dataset["Channel Name"].values)).map(
+        FeatureExtraction.tokens_containing_channel_name).pad(MAX_SEQUENCE_LENGTH).to_tensor()
+    features.append(feature_channel)
+
+    feature_description = TensorMonad(
+        (dataset["Original Tokens"].values, dataset["Description"].values)).map(
+        FeatureExtraction.count_token_occurrences).pad(MAX_SEQUENCE_LENGTH).to_tensor()
+    features.append(feature_description)
+
+    feature_token_length = TensorMonad(
+        [dataset["Original Tokens"].values]).map(
+        FeatureExtraction.length_of_tokens).pad(MAX_SEQUENCE_LENGTH).to_tensor()
+    features.append(feature_token_length)
+
+    feature_is_token_verbal = TensorMonad(
+        [dataset["Original Tokens"].values]).map(
+        FeatureExtraction.is_token_verbal).pad(MAX_SEQUENCE_LENGTH).to_tensor()
+    features.append(feature_is_token_verbal)
+
+    features = np.concatenate(features, axis=2)
+    dataset["Features"] = features.tolist()
+
+    train_df, validation_df = split_dataset(
+        dataset, fraction=0.8, random_state=42)
+
+    title_vectorizer = VectorizerKerasTokenizer(
+        VOCAB_SIZE, MAX_SEQUENCE_LENGTH)
+    title_vectorizer.train(train_df["Tokens"].values)
+    train_titles = title_vectorizer.encode_batch(train_df["Tokens"].values)
+    val_titles = title_vectorizer.encode_batch(validation_df["Tokens"].values)
+    train_ner = train_df["NER"].values
+
+    val_ner = validation_df["NER"].values
+    train_ner = np.array(list(train_ner))
+    val_ner = np.array(list(val_ner))
+
+    train_features = np.array(list(train_df["Features"].values))
+    val_features = np.array(list(validation_df["Features"].values))
+
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+        ((train_titles, train_features), train_ner)).shuffle(1000).batch(1024).prefetch(tf.data.AUTOTUNE)
+    val_dataset = tf.data.Dataset.from_tensor_slices(
+        ((val_titles, val_features), val_ner)).batch(1024).prefetch(tf.data.AUTOTUNE)
+
+    model = build_model(train_dataset, val_dataset)
+
+    results = evaluate_model(model, val_titles, val_features, val_ner)
+    print(results)
+
+    class ModelWrapper:
+        def __init__(self, model, title_tokenizer, title_vectorizer):
+            self.model = model
+            self.title_tokenizer = title_tokenizer
+            self.title_vectorizer = title_vectorizer
+            self.max_sequence_length = MAX_SEQUENCE_LENGTH
+
+        def predict(self, title, channel_name, description):
+            original_tokens = self.title_tokenizer.encode(title)
+            tokens = preprocess_tokens(original_tokens)
+            vector = self.title_vectorizer.encode(tokens)
+            channel_vector = TensorMonad([[original_tokens], [channel_name]]).map(
+                FeatureExtraction.tokens_containing_channel_name).pad(
+                self.max_sequence_length).to_tensor()
+
+            description_vector = TensorMonad(
+                [[original_tokens], [description]]).map(
+                FeatureExtraction.count_token_occurrences).pad(
+                self.max_sequence_length).to_tensor()
+
+            length_vector = TensorMonad(
+                [[original_tokens]]).map(
+                FeatureExtraction.length_of_tokens).pad(
+                self.max_sequence_length).to_tensor()
+
+            is_token_verbal_vector = TensorMonad(
+                [[original_tokens]]).map(
+                FeatureExtraction.is_token_verbal).pad(
+                self.max_sequence_length).to_tensor()
+
+            features = np.concatenate(
+                [channel_vector, description_vector, length_vector, is_token_verbal_vector], axis=2)
+
+            vector = np.array(vector, dtype=float)
+            vector = vector.reshape(1, -1)
+
+            predictions = self.model.predict([vector, features])
+            predictions = np.argmax(predictions, axis=-1)
+            return decode_prediction(predictions[0], original_tokens)
+
+    model_wrapper = ModelWrapper(model, title_tokenizer, title_vectorizer)
+    print(model_wrapper.predict(
+        "MISSH feat. BURAI – Budapest (Official Music Video) | #misshmusic",
+        "#MISSHMUSIC",
+        "#MISSHMUSIC\n\nhttps://open.spotify.com/artist/6PD6eSZM8ulCg5PRU6mEII\nhttps://music.apple.com/lt/artist/misshmusic/1282462090\nhttps://www.deezer.com/us/artist/13167869\n\nhttps://www.tiktok.com/@misshmusic_official\nhttps://www.facebook.com/MR.MISSH90\nhttps://www.instagram.com/missh90/\n\nKülönköszönet : Ecke22 étterem /www.ecke22etterem.hu                    \n\nA  MisshMusic  2013 óta működő független magyar zenei produkciós iroda. Fő tevékenységünk saját dalok és videoklipek készítése továbbá egyedi tervezésű saját márkás ruházati termékek forgalmazása. Eddigi zenei együttműködő partnereink: G.w.M. , Burai Krisztián, Young G26, IGNI, Hegyi Roland (HR)."
+    ))
+
+    with open('out/model.pkl', 'wb') as f:
+        dill.dump(model_wrapper, f)
 
 
-dataset = fix_dataset_NER(dataset)
-
-
-ner_tags = convert_ner_tags(dataset["Title"].values, dataset["NER"].values)
-dataset["NER"] = ner_tags
-
-
-title_tokenizer = TokenizerCustom()
-dataset["Original Tokens"] = dataset["Title"].apply(
-    lambda x: title_tokenizer.encode(x))
-dataset["Tokens"] = dataset["Original Tokens"].apply(
-    lambda x: preprocess_tokens(x))
-
-ner_vectorizer = VectorizerNER(MAX_SEQUENCE_LENGTH)
-ner_vectorizer.train(dataset["NER"].values)
-dataset["NER"] = dataset["NER"].apply(
-    lambda x: ner_vectorizer.encode(x))
-
-features = []
-
-feature_channel = TensorMonad(
-    (dataset["Original Tokens"].values, dataset["Channel Name"].values)).map(
-    FeatureExtraction.tokens_containing_channel_name).pad(MAX_SEQUENCE_LENGTH).to_tensor()
-features.append(feature_channel)
-
-feature_description = TensorMonad(
-    (dataset["Original Tokens"].values, dataset["Description"].values)).map(
-    FeatureExtraction.count_token_occurrences).pad(MAX_SEQUENCE_LENGTH).to_tensor()
-features.append(feature_description)
-
-feature_token_length = TensorMonad(
-    [dataset["Original Tokens"].values]).map(
-    FeatureExtraction.length_of_tokens).pad(MAX_SEQUENCE_LENGTH).to_tensor()
-features.append(feature_token_length)
-
-feature_is_token_verbal = TensorMonad(
-    [dataset["Original Tokens"].values]).map(
-    FeatureExtraction.is_token_verbal).pad(MAX_SEQUENCE_LENGTH).to_tensor()
-features.append(feature_is_token_verbal)
-
-features = np.concatenate(features, axis=2)
-dataset["Features"] = features.tolist()
-
-train_df, validation_df = split_dataset(dataset, fraction=0.8, random_state=42)
-
-
-title_vectorizer = VectorizerKerasTokenizer(VOCAB_SIZE, MAX_SEQUENCE_LENGTH)
-title_vectorizer.train(train_df["Tokens"].values)
-train_titles = title_vectorizer.encode_batch(train_df["Tokens"].values)
-val_titles = title_vectorizer.encode_batch(validation_df["Tokens"].values)
-train_ner = train_df["NER"].values
-
-val_ner = validation_df["NER"].values
-train_ner = np.array(list(train_ner))
-val_ner = np.array(list(val_ner))
-
-train_features = np.array(list(train_df["Features"].values))
-val_features = np.array(list(validation_df["Features"].values))
-
-train_dataset = tf.data.Dataset.from_tensor_slices(
-    ((train_titles, train_features), train_ner)).shuffle(1000).batch(1024).prefetch(tf.data.AUTOTUNE)
-val_dataset = tf.data.Dataset.from_tensor_slices(
-    ((val_titles, val_features), val_ner)).batch(1024).prefetch(tf.data.AUTOTUNE)
-
-model = build_model(train_dataset, val_dataset)
-
-results = evaluate_model(model, val_titles, val_features, val_ner)
-print(results)
-
-
-class ModelWrapper:
-    def __init__(self, model, title_tokenizer, title_vectorizer):
-        self.model = model
-        self.title_tokenizer = title_tokenizer
-        self.title_vectorizer = title_vectorizer
-        self.max_sequence_length = MAX_SEQUENCE_LENGTH
-
-    def predict(self, title, channel_name, description):
-        original_tokens = self.title_tokenizer.encode(title)
-        tokens = preprocess_tokens(original_tokens)
-        vector = self.title_vectorizer.encode(tokens)
-        channel_vector = TensorMonad([[original_tokens], [channel_name]]).map(
-            FeatureExtraction.tokens_containing_channel_name).pad(
-            self.max_sequence_length).to_tensor()
-
-        description_vector = TensorMonad(
-            [[original_tokens], [description]]).map(
-            FeatureExtraction.count_token_occurrences).pad(
-            self.max_sequence_length).to_tensor()
-
-        length_vector = TensorMonad(
-            [[original_tokens]]).map(
-            FeatureExtraction.length_of_tokens).pad(
-            self.max_sequence_length).to_tensor()
-
-        is_token_verbal_vector = TensorMonad(
-            [[original_tokens]]).map(
-            FeatureExtraction.is_token_verbal).pad(
-            self.max_sequence_length).to_tensor()
-
-        features = np.concatenate(
-            [channel_vector, description_vector, length_vector, is_token_verbal_vector], axis=2)
-
-        vector = np.array(vector, dtype=float)
-        vector = vector.reshape(1, -1)
-
-        predictions = self.model.predict([vector, features])
-        predictions = np.argmax(predictions, axis=-1)
-        return decode_prediction(predictions[0], original_tokens)
-
-
-model_wrapper = ModelWrapper(model, title_tokenizer, title_vectorizer)
-print(model_wrapper.predict(
-    "MISSH feat. BURAI – Budapest (Official Music Video) | #misshmusic",
-    "#MISSHMUSIC",
-    "#MISSHMUSIC\n\nhttps://open.spotify.com/artist/6PD6eSZM8ulCg5PRU6mEII\nhttps://music.apple.com/lt/artist/misshmusic/1282462090\nhttps://www.deezer.com/us/artist/13167869\n\nhttps://www.tiktok.com/@misshmusic_official\nhttps://www.facebook.com/MR.MISSH90\nhttps://www.instagram.com/missh90/\n\nKülönköszönet : Ecke22 étterem /www.ecke22etterem.hu                    \n\nA  MisshMusic  2013 óta működő független magyar zenei produkciós iroda. Fő tevékenységünk saját dalok és videoklipek készítése továbbá egyedi tervezésű saját márkás ruházati termékek forgalmazása. Eddigi zenei együttműködő partnereink: G.w.M. , Burai Krisztián, Young G26, IGNI, Hegyi Roland (HR)."
-))
-
-with open('out/model.pkl', 'wb') as f:
-    dill.dump(model_wrapper, f)
+if __name__ == "__main__":
+    main()
--- src/prod.py
+++ src/prod.py
@@ -7,22 +7,28 @@ from model import decode_prediction
 from text_cleaner import preprocess_tokens
 from tensormonad import TensorMonad
 
-with open('out/model.pkl', 'rb') as f:
-    model = dill.load(f)
 
-    app = Flask(__name__)
+def main():
+    with open('out/model.pkl', 'rb') as f:
+        model = dill.load(f)
 
-    @app.route('/predict', methods=['POST'])
-    def predict():
-        data = request.get_json(force=True)
-        title = data['title']
-        channel_name = data['channel_name']
-        description = data['description']
-        grouped_results = model.predict(title, channel_name, description)
-        return jsonify({
-            'prediction': grouped_results,
-            'status': 'success'
-        })
+        app = Flask(__name__)
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000, debug=True)
+        @app.route('/predict', methods=['POST'])
+        def predict():
+            data = request.get_json(force=True)
+            title = data['title']
+            channel_name = data['channel_name']
+            description = data['description']
+            grouped_results = model.predict(title, channel_name, description)
+            return jsonify({
+                'prediction': grouped_results,
+                'status': 'success'
+            })
+
+        if __name__ == '__main__':
+            app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
