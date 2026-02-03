237f318a04e19b5048a1a21c7d661b09fb677dfe
GitDevla <pataki.david.ferenc@gmail.com>
2026-02-03 14:37:50 +0100

    fix: dont remove \n tokens in TokenizerCustom

--- src/tokenizer/TokenizerCustom.py
+++ src/tokenizer/TokenizerCustom.py
@@ -6,20 +6,26 @@ class TokenizerCustom:
 
     def encode(self, text):
         matches = re.finditer(
-            r"([一-龠ぁ-ゔァ-ヴーａ-ｚＡ-Ｚ０-９々〆〤0-9゙゚]+|[a-zA-Z0-9]+)[.!]*", text
+            r"([一-龠ぁ-ゔァ-ヴーａ-ｚＡ-Ｚ０-９々〆〤0-9゙゚]+|[a-zA-Z0-9]+)[.!]*",
+            text,
         )
-        match_positions = [match.span() for match in matches]
+
         tokens = []
         current_pos = 0
-        for start, end in match_positions:
+
+        for match in matches:
+            start, end = match.span()
+
             if current_pos < start:
-                tokens.extend([*text[current_pos:start]])
+                tokens.extend(list(text[current_pos:start]))
+
             tokens.append(text[start:end])
             current_pos = end
-        if current_pos < len(text):
-            tokens.extend([*text[current_pos:]])
 
-        return [t.strip() for t in tokens if t.strip()]
+        if current_pos < len(text):
+            tokens.extend(list(text[current_pos:]))
+
+        return [t for t in tokens if t == "\n" or not t.isspace()]
 
     def encode_batch(self, texts):
         return [self.encode(text) for text in texts]
