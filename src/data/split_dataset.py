39d47535a12dd391ea18146ccc243b0d493eaf45
GitDevla <pataki.david.ferenc@gmail.com>
2026-02-23 20:54:34 +0100

    fix: adjust train-test split ratio from 0.2 to 0.1 for dataset

--- src/data/split_dataset.py
+++ src/data/split_dataset.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 def split_dataset() -> None:
     dataset = pd.read_json("dataset/videos.json")
-    train_df, val_df = train_test_split(dataset, test_size=0.2)
+    train_df, val_df = train_test_split(dataset, test_size=0.1)
 
     train_df.reset_index(drop=True)
     val_df.reset_index(drop=True)
